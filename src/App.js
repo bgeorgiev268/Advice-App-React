db76a1cf894d40fdf57790c229de498ffc10d312
bgeorgiev268 <bgeorgiev268@gmail.com>
2020-11-16 19:26:09 -0500

    edited onClick

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
 			<div className='app'>
 				<div className='card'>
 					<h1 className='heading'> {advice} </h1>
-					<button className='button' onClick={this.fetchAdvice()}>
+					<button className='button' onClick={this.fetchAdvice}>
 						<span>GIVE ME ADVICE!</span>
 					</button>
 				</div>
